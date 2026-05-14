d97d05b75d2b789b14ef014733331260d5d64e58
LongNH-git <longnguyenhai.ptit@gmail.com>
2026-05-14 20:39:10 +0700

    Fix

--- client/js/main.js
+++ client/js/main.js
@@ -31,3 +31,34 @@ if (user && user.role === "admin") {
 } else {
     adminLink.style.display = "none";
 }
+// Gear Store
+const user = JSON.parse(localStorage.getItem("user"));
+
+const loginLink = document.getElementById("loginLink");
+const registerLink = document.getElementById("registerLink");
+const userBox = document.getElementById("userBox");
+const userName = document.getElementById("userName");
+const logoutBtn = document.getElementById("logoutBtn");
+const adminLink = document.getElementById("adminLink");
+
+if (user) {
+  if (loginLink) loginLink.style.display = "none";
+  if (registerLink) registerLink.style.display = "none";
+
+  if (userBox) userBox.style.display = "flex";
+  if (userName) userName.innerText = user.name;
+
+  if (adminLink && user.role === "admin") {
+    adminLink.style.display = "inline-block";
+  }
+} else {
+  if (adminLink) adminLink.style.display = "none";
+}
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    window.location.href = "login.html";
+  });
+}
